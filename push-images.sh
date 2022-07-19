671e48765ccc074b9a5f83317aa2752bcce2df9b
Jeff Chen <jeff.chen@circleci.com>
2022-07-19 15:01:02 -0700

    testing cimg-orb

--- push-images.sh
+++ push-images.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/go:1.17
-docker push cimg/go:1.17.12
-docker push cimg/go:1.17-node
-docker push cimg/go:1.17.12-node
-docker push cimg/go:1.17-browsers
-docker push cimg/go:1.17.12-browsers
 docker push cimg/go:1.18
 docker push cimg/go:1.18.4
 docker push cimg/go:1.18-node
